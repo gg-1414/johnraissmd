6aff3a23a54fc0cc71ba809b7dfd9c4b85f640f2
github username <ginalee1114@gmail.com>
2019-03-18 13:13:24 -0400

    added react-reveal for text fade & reveal animation on scroll

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Content from './containers/Content';
 import Footer from './components/Footer';
 import './App.css';
+import Fade from 'react-reveal/Fade'
 
 
 class App extends Component {
@@ -20,8 +21,11 @@ class App extends Component {
     console.log('throttled')
     let scrollPosition = Math.round( window.scrollY );
 
-    if (scrollPosition > 250){ // if scrolled past 300px, add header--sticky-shrink class
+    if( scrollPosition > 250 ) { // if scrolled past 300px, add header--sticky-shrink class
       document.querySelector( 'header' ).classList.add( 'header--sticky-shrink' );
+      // if( scrollPosition > 300 ) {
+      //   console.log(document.querySelector( 'ul' ))
+      // }
     } else { // if not, remove header--sticky-shrink class from header
         document.querySelector( 'header' ).classList.remove( 'header--sticky-shrink' );
     }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import '../stylesheets/Content.css'
 import '../stylesheets/Home.css'
+import Fade from 'react-reveal/Fade';
+import Reveal from 'react-reveal/Reveal';
 
 const Home = () => (
   <div className="content">
-    <div className="clearfix home__header">
-      <span>Certified</span>
-      <h1>Child and Adult Psychiatry & Psychopharmacology</h1>
-    </div>
+    <Fade bottom>
+      <div className="clearfix home__header">
+        <span>Certified</span>
+        <h1>Child and Adult Psychiatry & Psychopharmacology</h1>
+      </div>
+    </Fade>
     <section className="home__description">
-      <h2 style={{ paddingLeft:'350px' }}>I am a licensed physician who is board certified in two medical specialties: general psychiatry and child & adolescent psychiatry.</h2>
-      <p>The disorders I diagnose and treat include:</p>
-      <ul>
-        <li><a href="#">Mood Disorders</a></li>
-        <li><a href="#">Anxiety Disorders</a></li>
-        <li><a href="#">Attention Deficit Hyperactivity Disorder</a></li>
-        <li><a href="#">Sleep Problems</a></li>
-        <li><a href="#">Psychotic Disorders</a></li>
-        <li><a href="#">Autistic Spectrum Disorders</a></li>
-			</ul>
+      <Fade bottom>
+        <h2 style={{ paddingLeft:'350px' }}>I am a licensed physician who is board certified in two medical specialties: general psychiatry and child & adolescent psychiatry.</h2>
+        <p>The disorders I diagnose and treat include:</p>
+      </Fade>
+        <ul>
+          <Reveal effect="fadeInUp">
+            <li><a href="#">Mood Disorders</a></li>
+            <li><a href="#">Anxiety Disorders</a></li>
+            <li><a href="#">Attention Deficit Hyperactivity Disorder</a></li>
+            <li><a href="#">Sleep Problems</a></li>
+            <li><a href="#">Psychotic Disorders</a></li>
+            <li><a href="#">Autistic Spectrum Disorders</a></li>
+          </Reveal>
+  			</ul>
     </section>
     <section className="home__message">
+      <Reveal effect="fadeInUp">
       <p>Like the rest of human life, the field of psychiatry is constantly changing. Most of the medications I use today were not in existence when I finished my residency. It is important for a psychiatrist to keep up with new developments. To this end, I read several journals and newsletters, and I obtain my continuing medical education hours from sources that are independent of the pharmaceutical industry.</p>
       <p>I look forward to meeting with you and delivering the highest quality care.</p>
       <p>John C. Raiss, MD</p>
+      </Reveal>
     </section>
     <section className="home__latestBlogs">
       <div>
